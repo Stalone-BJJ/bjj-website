2d8d75392a8a6ca462dce9c8baf71892b317ac8c
Hakan Cimen <hakan.cimen@pimberly.com>
2023-03-29 21:01:58 +0100

    created schedule page and added to nav bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -74,6 +74,14 @@ export const NavBar = () => {
             </li>
             <li>
               <Link
+                href="/timetable"
+                className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+              >
+                Schedule
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="/contact"
                 className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
               >
---
+++ src/pages/schedule.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Footer } from "~/components/footer";
+import { Heading } from "~/components/heading";
+import { NavBar } from "~/components/navbar";
+import { Subheading } from "~/components/subheading";
+
+const Schedule = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <div className="m-20 text-center">
+        <Heading headingText="Schedule" />
+        <Subheading subheadingText="Our Schedule and Class Selection" />
+      </div>
+      <div className="m-20 flex justify-center">
+        <p className="">Schedule</p>
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default Schedule;
