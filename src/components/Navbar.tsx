bd853b63220ec86fa99a8f1a583f17256b00fea5
iainbrux <me@iainbrux.com>
2023-04-23 23:03:51 +0100

    finished nav styling improvements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ export const NavBar = () => {
             src="/images/thumbnail_stalone-logo-1.png"
             width={100}
           />
-          <span className="my-4 ml-28 self-center whitespace-nowrap text-xl font-semibold dark:text-white lg:my-0 lg:ml-40">
+          <span className="my-4 ml-36 self-center whitespace-nowrap text-3xl font-semibold dark:text-white lg:my-0 lg:ml-40">
             STALONE BJJ ACADEMY
           </span>
         </Link>
@@ -58,7 +58,6 @@ export const NavBar = () => {
             <ul className="mt-4 flex flex-col items-center justify-center text-center">
               <li className="mb-4">
                 <Link
-                  aria-current="page"
                   className={`text-xl ${
                     pathName === "/" ? "font-bold text-red-600" : "text-white"
                   }`}
@@ -69,7 +68,6 @@ export const NavBar = () => {
               </li>
               <li className="mb-4">
                 <Link
-                  aria-current="page"
                   className={`text-xl ${
                     pathName === "/schedule"
                       ? "font-bold text-red-600"
@@ -82,7 +80,6 @@ export const NavBar = () => {
               </li>
               <li className="mb-4">
                 <Link
-                  aria-current="page"
                   className={`text-xl ${
                     pathName === "/contact"
                       ? "font-bold text-red-600"
@@ -97,14 +94,16 @@ export const NavBar = () => {
           </div>
         )}
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-black">
+          <ul className="mt-4 flex flex-col p-4 md:mt-0 md:flex-row md:space-x-8 md:font-medium">
             <li>
               <Link
                 aria-current="page"
-                className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                className={`${
+                  pathName === "/" ? "font-bold text-red-600" : "text-white"
+                } block rounded py-2 pl-3 pr-4 hover:text-red-600 md:border-0 md:p-0`}
                 href="/"
               >
-                Home
+                HOME
               </Link>
             </li>
             {/* <li>
@@ -134,18 +133,26 @@ export const NavBar = () => {
             </li> */}
             <li>
               <Link
-                className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                className={`${
+                  pathName === "/schedule"
+                    ? "font-bold text-red-600"
+                    : "text-white"
+                } block rounded py-2 pl-3 pr-4 hover:text-red-600 md:border-0 md:p-0`}
                 href="/schedule"
               >
-                Schedule
+                SCHEDULE
               </Link>
             </li>
             <li>
               <Link
-                className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                className={`${
+                  pathName === "/contact"
+                    ? "font-bold text-red-600"
+                    : "text-white"
+                } block rounded py-2 pl-3 pr-4 hover:text-red-600 md:border-0 md:p-0`}
                 href="/contact"
               >
-                Contact
+                CONTACT
               </Link>
             </li>
           </ul>
