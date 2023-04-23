c3ea768b8f0061296f81fe8c0e82de939293e24e
iainbrux <me@iainbrux.com>
2023-04-23 22:27:34 +0100

    fixed hamburger menu for mobile, moved about to toBuild folder as unsure how to proceed so check with stalone

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="sticky top-0 z-10 border-b-2 border-red-600 bg-black px-2 py-2.5 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
@@ -29,6 +32,7 @@ export const NavBar = () => {
           aria-expanded="false"
           className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
           data-collapse-toggle="navbar-default"
+          onClick={() => setShowMenu(!showMenu)}
           type="button"
         >
           <span className="sr-only">Open main menu</span>
@@ -46,6 +50,39 @@ export const NavBar = () => {
             ></path>
           </svg>
         </button>
+        {showMenu && (
+          <div className="w-full md:hidden" id="navbar-default">
+            <ul className="mt-4 flex flex-col items-center justify-center text-center">
+              <li className="mb-4">
+                <Link
+                  aria-current="page"
+                  className="text-xl text-white"
+                  href="/"
+                >
+                  HOME
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  aria-current="page"
+                  className="text-xl text-white"
+                  href="/schedule"
+                >
+                  SCHEDULE
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  aria-current="page"
+                  className="text-xl text-white"
+                  href="/contact"
+                >
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-black">
             <li>
@@ -57,14 +94,14 @@ export const NavBar = () => {
                 Home
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
                 className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                 href="/about"
               >
                 About
               </Link>
-            </li>
+            </li> */}
             {/* <li>
               <Link
                 aria-current="page"
