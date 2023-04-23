7ff85a8e3c6d51d4990eaaa0353bd3f593bce09a
iainbrux <me@iainbrux.com>
2023-04-23 23:05:06 +0100

    comment in to prevent build failures

--- src/toBuild/about.tsx
+++ src/toBuild/about.tsx
@@ -1,48 +1,48 @@
-// import React from "react";
-// import { Footer } from "~/components/Footer";
-// import { Heading } from "~/components/Heading";
-// import { NavBar } from "~/components/Navbar";
-// import { SectionHeading } from "~/components/SectionHeading";
-// import { Subheading } from "~/components/Subheading";
-// import { ContentSection } from "~/components/ContentSection";
-// import { Carousel } from "~/components/Carousel";
+import React from "react";
+import { Footer } from "~/components/Footer";
+import { Heading } from "~/components/Heading";
+import { NavBar } from "~/components/Navbar";
+import { SectionHeading } from "~/components/SectionHeading";
+import { Subheading } from "~/components/Subheading";
+import { ContentSection } from "~/components/ContentSection";
+import { Carousel } from "~/components/Carousel";
 
-// const About = () => {
-//   return (
-//     <React.Fragment>
-//       <NavBar />
-//       <div className="m-20 text-center">
-//         <Heading>About Us</Heading>
-//         <Subheading>This section is about us.</Subheading>
-//       </div>
-//       <div className="m-20 flex ">
-//         <ContentSection>
-//           <SectionHeading sectionHeadingText="Our Instructors" />
-//           <div className="flex">
-//             <Carousel></Carousel>
-//           </div>
-//         </ContentSection>
-//         <ContentSection>
-//           <SectionHeading sectionHeadingText="Look at this armbar" />
-//           <div className="flex">
-//             <p>
-//               Lorem Ipsum is simply dummy text of the printing and typesetting
-//               industry. Lorem Ipsum has been the industrys standard dummy text
-//               ever since the 1500s, when an unknown printer took a galley of
-//               type and scrambled it to make a type specimen book. It has
-//               survived not only five centuries, but also the leap into
-//               electronic typesetting, remaining essentially unchanged. It was
-//               popularised in the 1960s with the release of Letraset sheets
-//               containing Lorem Ipsum passages, and more recently with desktop
-//               publishing software like Aldus PageMaker including versions of
-//               Lorem Ipsum.
-//             </p>
-//           </div>
-//         </ContentSection>
-//       </div>
-//       <Footer />
-//     </React.Fragment>
-//   );
-// };
+const About = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <div className="m-20 text-center">
+        <Heading>About Us</Heading>
+        <Subheading>This section is about us.</Subheading>
+      </div>
+      <div className="m-20 flex ">
+        <ContentSection>
+          <SectionHeading sectionHeadingText="Our Instructors" />
+          <div className="flex">
+            <Carousel></Carousel>
+          </div>
+        </ContentSection>
+        <ContentSection>
+          <SectionHeading sectionHeadingText="Look at this armbar" />
+          <div className="flex">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+          </div>
+        </ContentSection>
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-// export default About;
+export default About;
